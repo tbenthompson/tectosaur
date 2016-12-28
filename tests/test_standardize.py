889d7e8f89af2f2e96343c975d30116ac93b860c
Ben Thompson <t.ben.thompson@gmail.com>
2016-12-27 23:19:11 -0500

    Reorganizing tables code.

--- tests/test_standardize.py
+++ tests/test_standardize.py
@@ -1,4 +1,9 @@
 from tectosaur.standardize import *
+import tectosaur.quadrature as quad
+import tectosaur.limit as limit
+import cppimport
+adaptive_integrate = cppimport.imp('tectosaur.tables.adaptive_integrate').tables.adaptive_integrate
+
 
 def test_origin_vertex():
     assert(get_origin_vertex(get_edge_lens(np.array([[0,0,0],[1,0,0],[0.2,0.5,0]]))) == 0)
@@ -47,3 +52,138 @@ def test_standardize():
     # np.testing.assert_almost_equal(out, [[0,0,0],[0.4,0.5,0],[0.2,0,0]])
     out,_,_,_,_ = standardize(np.array([[0,0,0],[1,0.0,0],[0.0,0.5,0]]), 20)
     np.testing.assert_almost_equal(out, [[0,0,0],[1.0,0.0,0],[0.2,0.4,0]])
+
+def co_integrals(K, tri, rho_order, theta_order, tol, eps_start, n_steps, sm, pr):
+    epsvs = eps_start * (2.0 ** (-np.arange(n_steps)))
+    vals = []
+    for eps in epsvs:
+        rho_gauss = quad.gaussxw(rho_order)
+        rho_q = quad.sinh_transform(rho_gauss, -1, eps * 2)
+        theta_q = quad.gaussxw(theta_order)
+        vals.append(adaptive_integrate.integrate_coincident(
+            K, tri, tol, eps, sm, pr,
+            rho_q[0].tolist(), rho_q[1].tolist(),
+            theta_q[0].tolist(), theta_q[1].tolist()
+        ))
+    vals = np.array(vals)
+    return epsvs, vals
+
+def adj_integrals(K, obs_tri, src_tri, rho_order, theta_order, tol, eps_start, n_steps, sm, pr):
+    epsvs = eps_start * (2.0 ** (-np.arange(n_steps)))
+    vals = []
+    for eps in epsvs:
+        rho_gauss = quad.gaussxw(rho_order)
+        rho_q = quad.sinh_transform(rho_gauss, -1, eps * 2)
+        vals.append(adaptive_integrate.integrate_adjacent(
+            K, obs_tri, src_tri, tol, eps,
+            sm, pr, rho_q[0].tolist(), rho_q[1].tolist()
+        ))
+
+    vals = np.array(vals)
+    return epsvs, vals
+
+def take_limits(epsvs, vals, include_log):
+    return np.array([limit.limit(epsvs, vals[:, i], include_log) for i in range(81)])
+
+# To test whether the standardization procedure and the transform_from_standard function
+# are working properly, I compare the results of a direct integration with the results
+# of a standardized triangle integration and subsequent unstandardization.
+# There are some tricky/key pieces here. In particular, the starting epsilon value must be
+# multiplied by (scale ** 2) in order to match the scaling of the triangle area.
+# This is not strictly necessary, but avoiding it would require much higher accuracy and lower
+# epsilon values before the scaled and unscaled integrals matched each other (in the limit,
+# of course, they should match!)
+def standardized_tri_tester(K, sm, pr, rho_order, theta_order, tol, eps_start, n_steps, tri):
+    include_log = True
+    standard_tri, labels, translation, R, scale = standardize(
+        np.array(tri), 20, True
+    )
+    is_flipped = not (labels[1] == ((labels[0] + 1) % 3))
+
+    np.testing.assert_almost_equal(
+        standard_tri,
+        [[0,0,0],[1,0,0],[standard_tri[2][0],standard_tri[2][1],0]]
+    )
+
+    correct_epsvs, correct_vals = co_integrals(
+        K, tri, rho_order, theta_order, tol, eps_start, n_steps, sm, pr
+    )
+    correct_limits = take_limits(correct_epsvs, correct_vals, include_log)
+    correct_limits = correct_limits[:,0].reshape((3,3,3,3))
+
+    # 1) calculate the standardized integrals
+    epsvs, standard_vals = co_integrals(
+        K, standard_tri, rho_order, theta_order, tol, eps_start * scale, n_steps, 1.0, pr
+    )
+
+    # 2) convert them to the appropriate values for true triangles
+    unstandardized_vals = np.array([
+        np.array(transform_from_standard(
+            # standard_vals[i,:], K, sm, labels, translation, R, scale
+            standard_vals[i,:], K, sm, labels, translation, R, scale
+        )).reshape(81)
+        for i in range(standard_vals.shape[0])
+    ])
+
+    # 3) take the limit in true space, not standardized space
+    unstandardized_limits = take_limits(epsvs / scale, unstandardized_vals, include_log)
+    unstandardized_limits = unstandardized_limits[:,0].reshape((3,3,3,3))
+
+    A = unstandardized_limits[0,0,0,0]
+    B = correct_limits[0,0,0,0]
+
+    print(
+        str(tol) +
+        " " + str(eps_start) +
+        " " + str(n_steps) +
+        " " + str(A) +
+        " " + str(B)
+    )
+    err = np.abs((unstandardized_limits[:,0,:,0] - correct_limits[:,0,:,0]) / np.max(np.abs(correct_limits[:,0,:,0])))
+    assert(np.all(err < 0.03))
+    # np.testing.assert_almost_equal(unstandardized_limits, correct_limits, 4)
+
+def kernel_properties_tester(K, sm, pr):
+    test_tris = [
+        # [[0.0,0.0,0.0], [1.0,0.0,0.0], [0.4,0.3,0.0]], #NO TRANSFORMATION
+        [[0.0,0.0,0.0], [2.0,0.0,0.0], [0.8,0.6,0.0]], # JUST SCALE
+        [[0.0,0.0,0.0], [0.0,1.0,0.0], [-0.3,0.4,0.0]], # JUST ROTATE
+        [[0.0,0.0,0.0], [0.0,1.1,0.0], [-0.3,0.4,0.0]], # ROTATE + SCALE
+        [[0.0,0.0,0.0], [0.0,0.0,1.1], [0.0,-0.3,0.4]], # ROTATE + SCALE
+        [[0.0,0.0,0.0], [0.0,0.3,1.1], [0.0,-0.3,0.4]],
+        [[0.0,0.0,0.0], [0.0,-0.3,0.4], [0.0,0.35,1.1]],
+        [[0.0,0.35,1.1], [0.0,0.0,0.0], [0.0,-0.3,0.4]],
+        [[0.0, -0.3, 0.4], [0.0,0.35,1.1], [0.0,0.0,0.0]],
+        [[1.0,0.0,0.0], [0.0,-0.3,0.45], [0.0,0.35,1.1]]
+    ]
+    for t in test_tris:
+        print("TESTING " + str(t))
+        standardized_tri_tester(K, sm, pr, 50, 50, 0.005, 0.08, 3, t)
+        print("SUCCESS")
+
+    # n_checks = 10
+    # while True:
+    #     tri = np.random.rand(3,3).tolist()
+
+    #     try:
+    #         test_tri(tri)
+    #         print("SUCCESS!")
+    #     except Exception as e:
+    #         # Exception implies the triangle was malformed (angle < 20 degrees)
+    #         continue
+
+    #     n_checks -= 1
+    #     if n_checks == 0:
+    #         break
+
+def test_U_properties():
+    kernel_properties_tester('U', 1.0, 0.25)
+
+def test_T_properties():
+    kernel_properties_tester('T', 1.0, 0.25)
+
+def test_A_properties():
+    kernel_properties_tester('A', 1.0, 0.25)
+
+def test_H_properties():
+    kernel_properties_tester('H', 1.0, 0.25)
