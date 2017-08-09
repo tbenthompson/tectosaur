ff8ffb2d77e0647631123bdb056ff84f518f3fc5
Ben Thompson <t.ben.thompson@gmail.com>
2017-08-09 14:35:48 -0400

    Revert bounding sphere change.

--- tectosaur/fmm/tree_helpers.hpp
+++ tectosaur/fmm/tree_helpers.hpp
@@ -7,57 +7,28 @@ struct PtWithIdx {
     size_t orig_idx;
 };
 
-// template <size_t dim>
-// Ball<dim> tree_bounds(PtWithIdx<dim>* pts, size_t n_pts) {
-//     std::array<double,dim> com{};
-// 
-//     for (size_t i = 0; i < n_pts; i++) {
-//         for (size_t d = 0; d < dim; d++) {
-//             com[d] += pts[i].pt[d];
-//         }
-//     }
-//     for (size_t d = 0; d < dim; d++) {
-//         com[d] /= n_pts;
-//     }
-// 
-//     double max_r2 = 0.0;
-//     for (size_t i = 0; i < n_pts; i++) {
-//         for (size_t d = 0; d < dim; d++) {
-//             max_r2 = std::max(max_r2, dist2(pts[i].pt, com));
-//         }
-//     }
-// 
-//     return {com, std::sqrt(max_r2)};
-// }
-
-template <size_t dim>
-std::pair<size_t,double> furthest_pt(PtWithIdx<dim>* pts, size_t n_pts,
-    const std::array<double,dim>& from)
-{
-    size_t furthest_idx = 0;
-    double furthest_dist2 = dist2(from, pts[furthest_idx].pt);
-    for (size_t i = 1; i < n_pts; i++) {
-        double d2 = dist2(from, pts[i].pt);
-        if (d2 > furthest_dist2) {
-            furthest_idx = i;
-            furthest_dist2 = d2;
-        }
-    }
-    return {furthest_idx, furthest_dist2};
-}
-
+// Note: I tried using a minimum bounding sphere algorithm. This didn't help and actually hurt a little bit(?!). Furthermore, the tree construction cost went up quite a lot. On second thought, this makes sense, because there's probably some value in having the center of a node at its center of mass for the sake of the error in the translation operator. Also, for most nodes, the center of mass approach will be quite close to the minimum bounding sphere.
 template <size_t dim>
 Ball<dim> tree_bounds(PtWithIdx<dim>* pts, size_t n_pts) {
-    if (n_pts == 1) {
-        return {pts[0].pt, 0};
+    std::array<double,dim> com{};
+
+    for (size_t i = 0; i < n_pts; i++) {
+        for (size_t d = 0; d < dim; d++) {
+            com[d] += pts[i].pt[d];
+        }
     }
-    auto x = pts[0].pt;
-    auto y = furthest_pt(pts, n_pts, x);
-    auto z = furthest_pt(pts, n_pts, pts[y.first].pt);
-    auto center = mult(add(pts[y.first].pt, pts[z.first].pt), 0.5);
-    auto w = furthest_pt(pts, n_pts, center);
-    auto R = std::sqrt(w.second);
-    return {center, R};
+    for (size_t d = 0; d < dim; d++) {
+        com[d] /= n_pts;
+    }
+
+    double max_r2 = 0.0;
+    for (size_t i = 0; i < n_pts; i++) {
+        for (size_t d = 0; d < dim; d++) {
+            max_r2 = std::max(max_r2, dist2(pts[i].pt, com));
+        }
+    }
+
+    return {com, std::sqrt(max_r2)};
 }
 
 template <size_t dim>
