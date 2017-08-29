39a91d4cabd3bbd34bcecc475c185b7ac16a0f2e
Ben Thompson <t.ben.thompson@gmail.com>
2017-08-28 23:36:30 -0400

    simplify cpp testing

--- tests/fmm/test_main.cpp
+++ tests/fmm/test_main.cpp
@@ -6,21 +6,11 @@ fmm_test_cfg(cfg)
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(test_main) {
     py::module m("test_main");
-
-    m.def("run_tests", [] (std::vector<std::string> str_args) { 
-        char** argv = new char*[str_args.size()];
-        for (size_t i = 0; i < str_args.size(); i++) {
-            argv[i] = const_cast<char*>(str_args[i].c_str());
-        }
-        main(str_args.size(), argv); 
-        delete[] argv;
-    });
-
+    m.def("run_tests", [] () { main(0, nullptr); });
     return m.ptr();
 }
--- tests/fmm/test_run_cpp_tests.py
+++ tests/fmm/test_run_cpp_tests.py
@@ -2,5 +2,4 @@ import sys
 
 def test_run_cpp_tests():
     from cppimport import cppimport
-    test_main = cppimport('test_main')
-    test_main.run_tests(sys.argv)
+    cppimport('test_main').run_tests()
