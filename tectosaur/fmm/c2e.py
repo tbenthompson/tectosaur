Fixed numerical bug in the regularization of the FMM C2E operator.

tectosaur/fmm/c2e.py
@@ -11,7 +11,7 @@ class Ball:
 def inscribe_surf(ball, scaling, surf):
     return surf * ball.R * scaling + ball.center
 
-def c2e_solve(gpu_module, surf, bounds, check_r, equiv_r, K, params, float_type):
+def c2e_solve(gpu_module, surf, bounds, check_r, equiv_r, K, params, alpha, float_type):
     equiv_surf = inscribe_surf(bounds, equiv_r, surf)
     check_surf = inscribe_surf(bounds, check_r, surf)
 
@@ -19,8 +19,12 @@ def c2e_solve(gpu_module, surf, bounds, check_r, equiv_r, K, params, float_type)
         gpu_module, K, check_surf, surf, equiv_surf, surf, params, float_type
     )
 
-    rcond = np.finfo(float_type).eps * 30
-    out = np.linalg.pinv(equiv_to_check, rcond = rcond).flatten()
+    # A tikhonov regularization least squares solution via the SVD eigenvalue
+    # relation.
+    U, eig, VT = np.linalg.svd(equiv_to_check)
+    inv_eig = eig / (eig ** 2 + alpha ** 2)
+    out = (VT.T * inv_eig).dot(U.T).flatten()
+
     return out
 
 def direct_matrix(gpu_module, K, obs_pts, obs_ns, src_pts, src_ns, params, float_type):
@@ -46,7 +50,7 @@ def build_c2e(tree, check_r, equiv_r, cfg):
         return c2e_solve(
             cfg.gpu_module, cfg.surf,
             Ball([0] * cfg.K.spatial_dim, R), check_r, equiv_r,
-            cfg.K, cfg.params, cfg.float_type
+            cfg.K, cfg.params, cfg.alpha, cfg.float_type
         )
 
     n_rows = cfg.K.tensor_dim * cfg.surf.shape[0]

tests/fmm/test_fmm.py
@@ -4,6 +4,7 @@ import numpy as np
 import taskloaf as tsk
 from tectosaur.farfield import farfield_pts_direct
 from tectosaur.util.timer import Timer
+from tectosaur.util.test_decorators import slow
 import tectosaur.fmm.fmm as fmm
 from tectosaur.fmm.c2e import direct_matrix
 
@@ -11,8 +12,6 @@ from dimension import dim
 
 import logging
 logger = logging.getLogger(__name__)
-
-float_type = np.float64
 
 def rand_pts(dim):
     def f(n, source):
@@ -48,7 +47,9 @@ def get_pts(pts_builder, n):
     src_ns /= np.linalg.norm(src_ns, axis = 1)[:,np.newaxis]
     return obs_pts, obs_ns, src_pts, src_ns
 
-def run_full(n, pts_builder, mac, order, kernel, params, max_pts_per_cell = None):
+def run_full(n, pts_builder, mac, order, kernel, params,
+        float_type = np.float64, max_pts_per_cell = None):
+
     if max_pts_per_cell is None:
         max_pts_per_cell = order
     t = Timer()
@@ -90,7 +91,8 @@ def check(est, correct, accuracy):
     rhs = correct / rms_c
     np.testing.assert_almost_equal(lhs, rhs, accuracy)
 
-def check_kernel(K, obs_pts, obs_ns, src_pts, src_ns, est, accuracy = 3):
+def check_kernel(K, obs_pts, obs_ns, src_pts, src_ns, est,
+        float_type = np.float64, accuracy = 3):
     dim = obs_pts.shape[1]
     tensor_dim = int(est.size / obs_pts.shape[0])
     vec = np.ones(src_pts.shape[0] * tensor_dim)
@@ -132,6 +134,17 @@ def test_elastic():
         4000, rand_pts(dim), 2.6, order, K, [1.0, 0.25]
     ), accuracy = 1)
 
+@slow
+def test_elasticT_and_floattypes():
+    np.random.seed(10)
+    dim = 3
+    K = 'elasticU' + str(dim)
+    order = 16 if dim == 2 else 64
+    for ft in [np.float32, np.float64]:
+        check_kernel(K, *run_full(
+            20000, rand_pts(dim), 2.6, order, K, [1.0, 0.25], float_type = ft
+        ), float_type = ft, accuracy = 3)
+
 def test_m2l(laplace_kernel, dim):
     K = laplace_kernel + str(dim)
     order = 15 if dim == 2 else 100
@@ -148,6 +161,7 @@ def test_direct_matrix():
     K_name = "elasticU3"
     obs_pts, obs_ns, src_pts, src_ns = get_pts(rand_pts(3), 100)
     params = np.array([1.0, 0.25])
+    float_type = np.float64
     cfg = fmm.make_config(K_name, params, 1.0, 1.0, 1, float_type)
     matrix = direct_matrix(cfg.gpu_module, cfg.K, obs_pts, obs_ns, src_pts, src_ns, params, float_type)
     est = matrix.dot(np.ones(matrix.shape[1]))

tests/fmm/test_translation.py
@@ -17,8 +17,8 @@ def test_c2e_solve(request):
     s = surrounding_surf(50, 3)
     K_name = 'elasticH3'
     params = np.array([1.0, 0.25])
-    cfg = fmm.make_config(K_name, params, 1.0, 1.0, 1, float_type)
-    op = c2e_solve(cfg.gpu_module, s, b, 3.0, 1.1, cfg.K, params, float_type)
+    cfg = fmm.make_config(K_name, params, 1.0, 1.0, 1, float_type, alpha = 1e-10)
+    op = c2e_solve(cfg.gpu_module, s, b, 3.0, 1.1, cfg.K, params, cfg.alpha, float_type)
     return op
 
 def test_scaling_relations():
@@ -26,15 +26,19 @@ def test_scaling_relations():
     s = surrounding_surf(5, 2)
 
     K_name = 'laplaceD2'
-    cfg = fmm.make_config(K_name, [], 1.0, 1.0, 1, float_type)
-    op = np.array(c2e_solve(cfg.gpu_module, s, b, 3.0, 1.1, cfg.K, cfg.params, float_type))
+    cfg = fmm.make_config(K_name, [], 1.0, 1.0, 1, float_type, alpha = 1e-8)
+    op = np.array(c2e_solve(cfg.gpu_module, s, b, 3.0, 1.1, cfg.K, cfg.params, cfg.alpha, float_type))
 
     for i in range(5):
         b2 = Ball(np.random.rand(2).tolist(), 1.0)
-        op2 = np.array(c2e_solve(cfg.gpu_module, s, b2, 3.0, 1.1, cfg.K, cfg.params, float_type))
+        op2 = np.array(c2e_solve(
+            cfg.gpu_module, s, b2, 3.0, 1.1, cfg.K, cfg.params, cfg.alpha, float_type
+        ))
         np.testing.assert_almost_equal(op2, op)
 
     scale = 2.0
     b2 = Ball([0,0], scale)
-    op2 = np.array(c2e_solve(cfg.gpu_module, s, b2, 3.0, 1.1, cfg.K, cfg.params, float_type)) / scale
+    op2 = np.array(c2e_solve(
+        cfg.gpu_module, s, b2, 3.0, 1.1, cfg.K, cfg.params, cfg.alpha, float_type
+    )) / scale
     np.testing.assert_almost_equal(op2, op)