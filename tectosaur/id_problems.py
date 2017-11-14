2a2422829b6a85c56857a67d4dd02679b3f54850
Ben Thompson <t.ben.thompson@gmail.com>
2017-11-14 13:38:58 -0500

    check for intersections!

--- tectosaur/id_problems.py
+++ tectosaur/id_problems.py
@@ -5,11 +5,9 @@ from tectosaur.nearfield.table_params import table_min_internal_angle, min_inter
 from tectosaur.util.cpp import imp
 edge_adj_setup = imp('tectosaur.nearfield.edge_adj_setup')
 standardize = imp('tectosaur.nearfield.standardize')
+tri_tri_intersect = imp('tectosaur.util.tri_tri_intersect')
 
-def check_edge_adj_sizes(pts, tris, ea):
-    pass
-
-def check_min_adj_angle(m):
+def check_min_adj_angle(m, ea = None):
     pts, tris = m
     close_or_touch_pairs = find_near_adj.find_close_or_touching(pts, tris, 2.0)
     nearfield_pairs, va, ea = find_near_adj.split_adjacent_close(close_or_touch_pairs, tris)
@@ -24,7 +22,6 @@ def check_min_adj_angle(m):
             continue
         bad_pairs.append(pair)
     return np.array(bad_pairs)
-
 
 def check_for_slivers(m):
     pts, tris = m
@@ -47,3 +44,72 @@ def check_tris_tall_enough(m):
         if not edge_adj_setup.check_xyhat(xyhat):
             bad_tris.append(i)
     return bad_tris
+
+def check_for_intersections_nearfield(pts, tris, nearfield_pairs):
+    if nearfield_pairs.shape[0] == 0:
+        return []
+    unique_near_pairs = np.unique(np.sort(nearfield_pairs, axis = 1), axis = 0)
+    bad_pairs = []
+    for pair in unique_near_pairs:
+        tri1 = pts[tris[pair[0]]].tolist()
+        tri2 = pts[tris[pair[1]]].tolist()
+        I = tri_tri_intersect.tri_tri_intersect(tri1, tri2)
+        if I:
+            bad_pairs.append(pair)
+    return bad_pairs
+
+def check_for_intersections_va(pts, tris, va):
+    if va.shape[0] == 0:
+        return []
+    unique_va = np.unique(np.hstack((np.sort(va[:,:2], axis = 1), va[:,2:])), axis = 0)
+    bad_pairs = []
+    for pair in unique_va:
+        tri1 = pts[tris[pair[0]]]
+
+        for edge in [1,2]:
+            shared_pt_tri1 = pair[2]
+            tri1[0] += (tri1[edge] - tri1[0]) * 0.01
+
+            tri2 = pts[tris[pair[1]]].tolist()
+            I = tri_tri_intersect.tri_tri_intersect(tri1.tolist(), tri2)
+            if I:
+                bad_pairs.append(pair[:2])
+                break
+    return bad_pairs
+
+def check_for_intersections_ea(pts, tris, ea):
+    if ea.shape[0] == 0:
+        return []
+    unique_ea = np.unique(np.hstack((np.sort(ea[:,:2], axis = 1), ea[:,2:])), axis = 0)
+    bad_pairs = []
+    for pair in unique_ea:
+        for d in range(3):
+            if tris[pair[0],d] in tris[pair[1]]:
+                continue
+            dist = np.sqrt(np.sum((pts[tris[pair[1]]] - pts[tris[pair[0],d]]) ** 2, axis = 1))
+            if np.any(dist == 0):
+                bad_pairs.append(pair)
+    return bad_pairs
+
+def check_for_intersections(m):
+    pts, tris = m
+    close_or_touch_pairs = find_near_adj.find_close_or_touching(pts, tris, 2.0)
+    nearfield_pairs, va, ea = find_near_adj.split_adjacent_close(close_or_touch_pairs, tris)
+
+    bad_pairs = []
+
+    # Three situations:
+    # 1) Nearfield pair is intersection
+    bad_pairs.extend(check_for_intersections_nearfield(pts, tris, nearfield_pairs))
+
+    # 2) Vertex adjacent pair actually intersects beyond just the vertex
+    # We can test for this by moving the shared vertex short distance
+    # along one of the edges of the first triangle. If there is still
+    # an intersection, then the triangles intersect at locations besides
+    # just the shared vertex.
+    bad_pairs.extend(check_for_intersections_va(pts, tris, va))
+
+    # 3) Edge adjacent pair is actually coincident. <-- Easy!
+    bad_pairs.extend(check_for_intersections_ea(pts, tris, ea))
+
+    return np.array(bad_pairs)
--- tests/test_id_problems.py
+++ tests/test_id_problems.py
@@ -17,9 +17,42 @@ def test_check_for_slivers():
     assert(bad_tris == [2, 4])
 
 # Also check for almost intersections where the triangles are really close?
-# https://stackoverflow.com/questions/7113344/find-whether-two-triangles-intersect-or-not
-def test_check_for_intersections():
-    pass
+def test_check_for_intersections_nearfield():
+    pts = np.array([
+        [0,0,0],[1,0,0],[0,1,0],
+        [0.1,-0.1,0],[0.5,0.5,0.5],[0.5,0.5,-0.5],
+    ])
+    tris = np.array([[0,1,2],[3,4,5]])
+    bad_pairs = id_problems.check_for_intersections((pts, tris))
+    assert(np.all(bad_pairs == [(0,1)]))
+
+def test_check_for_intersections_va():
+    pts = np.array([
+        [0,0,0],[1,0,0],[0,1,0],
+        [0.5,0.5,0.5],[0.5,0.5,-0.5],
+        [-1,0,0],[0,-1,0],
+    ])
+    tris = np.array([[0,1,2],[0,3,4],[0,5,6]])
+    bad_pairs = id_problems.check_for_intersections((pts, tris))
+    assert(np.all(bad_pairs == [(0,1)]))
+
+def test_check_for_intersections_va_inplane():
+    pts = np.array([
+        [0,0,0],[1,0,0],[0,1,0],
+        [0.5,0.5,0.5],[0.5,0.5,0.0],
+    ])
+    tris = np.array([[0,1,2],[0,3,4]])
+    bad_pairs = id_problems.check_for_intersections((pts, tris))
+    assert(np.all(bad_pairs == [(0,1)]))
+
+def test_check_for_intersections_ea():
+    pts = np.array([
+        [0,0,0],[1,0,0],[0,1,0],
+        [0,-1,0],[0,1,0]
+    ])
+    tris = np.array([[0,1,2],[0,1,3],[0,1,4]])
+    bad_pairs = id_problems.check_for_intersections((pts, tris))
+    assert(np.all(bad_pairs == [(0,2)]))
 
 def test_check_for_min_edge_adj_angle():
     pts = np.array([[0,0,0],[1,0,0],[0.5,0.5,0],[0.5, 0.4, 0.1],[0.5,-0.5,0.0]])
