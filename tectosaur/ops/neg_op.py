18787b2bfc69071c5dc59a3bc928e4d0350e0d74
Ben Thompson <t.ben.thompson@gmail.com>
2018-08-15 13:53:44 -0400

    full T*, A*, H* work with dense operator

--- tectosaur/ops/neg_op.py
+++ tectosaur/ops/neg_op.py
@@ -1,16 +1,21 @@
-class NegOp:
-    def __init__(self, op):
+class MultOp:
+    def __init__(self, op, factor):
         self.op = op
         self.shape = op.shape
+        self.factor = factor
 
     def nearfield_dot(self, v):
-        return -self.op.nearfield_dot(v)
+        return self.factor * self.op.nearfield_dot(v)
 
     def nearfield_no_correction_dot(self, v):
-        return -self.op.nearfield_no_correction_dot(v)
+        return self.factor * self.op.nearfield_no_correction_dot(v)
 
     def dot(self, v):
-        return -self.op.dot(v)
+        return self.factor * self.op.dot(v)
 
     def farfield_dot(self, v):
-        return -self.op.farfield_dot(v)
+        return self.factor * self.op.farfield_dot(v)
+
+#TODO: Deprecate
+def NegOp(op):
+    return MultOp(op, -1)
--- tests/test_regularized.py
+++ tests/test_regularized.py
@@ -6,30 +6,38 @@ from tectosaur.ops.dense_integral_op import RegularizedDenseIntegralOp
 from tectosaur.ops.sparse_farfield_op import \
         TriToTriDirectFarfieldOp, PtToPtDirectFarfieldOp, PtToPtFMMFarfieldOp
 from tectosaur.ops.mass_op import MassOp
+from tectosaur.ops.neg_op import MultOp
+from tectosaur.ops.sum_op import SumOp
 from tectosaur.nearfield.nearfield_op import any_nearfield
 from tectosaur.util.test_decorators import kernel
 
 def plot_fnc(m, surf1_idxs, surf2_idxs, slip, outs):
     def plot_at_pts(idxs, f):
-        pts_f = np.zeros(m[0].shape[0])
+        pts_f = np.full(m[0].shape[0], np.nan)
         pts_f[m[1][idxs]] = f
+
+        pts_f_not_nan = pts_f[np.logical_not(np.isnan(pts_f))]
+        min_f = np.min(pts_f_not_nan)
+        max_f = np.max(pts_f_not_nan)
+
         plt.figure()
         plt.tricontourf(
             m[0][:,0], m[0][:,2], m[1], pts_f,
-            # levels = np.linspace(-8, -4, 21),
+            levels = np.linspace(min_f, max_f, 21),
             # extend = 'both'
         )
         plt.colorbar()
 
     # plot_at_pts(surf2_idxs, np.log10(np.abs(slip[:,0].reshape((-1,3))) + 1e-40))
 
-    for o in outs:
-        plot_at_pts(surf1_idxs, np.log10(np.abs(o.reshape(-1,3,3)[:,:,1]) + 1e-40))
-    plt.show()
+    for d in range(3):
+        for o in outs:
+            plot_at_pts(surf1_idxs, np.abs(o.reshape(-1,3,3)[:,:,d] + 1e-40))
+        plt.show()
 
 
 
-def regularized_tester(K, sep, continuity):
+def regularized_tester(K, sep, continuity, mass_op_factor = 0.0):
     n_m = 30
     full_K_name = f'elastic{K}3'
     full_RK_name = f'elasticR{K}3'
@@ -46,6 +54,7 @@ def regularized_tester(K, sep, continuity):
     sparse_ops = [
         (PtToPtDirectFarfieldOp, full_K_name),
     ]
+
     if not nearfield:
         def change_K_tri_tri(*args):
             args = list(args)
@@ -64,11 +73,27 @@ def regularized_tester(K, sep, continuity):
             src_subset = surf2_idxs
         ) for C, Kn in sparse_ops
     ]
-    ops.append(RegularizedDenseIntegralOp(
-        6, 2, nq_near, 2.0, full_RK_name, [1.0, 0.25], m[0], m[1],
-        np.float32, obs_subset = surf1_idxs,
-        src_subset = surf2_idxs
-    ))
+    ops.append(SumOp([
+        RegularizedDenseIntegralOp(
+            6, 2, nq_near, 2.0, full_RK_name, [1.0, 0.25], m[0], m[1],
+            np.float32, obs_subset = surf1_idxs,
+            src_subset = surf2_idxs
+        ),
+        MultOp(MassOp(3, m[0], m[1][surf1_idxs]), mass_op_factor)
+    ]))
+
+    # def change_K_tri_tri(*args):
+    #     args = list(args)
+    #     args[1] = full_K_name
+    #     return PtToPtFMMFarfieldOp(150, 2.5, 5)
+    # ops.append(SumOp([
+    #     RegularizedDenseIntegralOp(
+    #         6, 2, nq_near, 2.0, full_RK_name, [1.0, 0.25], m[0], m[1],
+    #         np.float32, obs_subset = surf1_idxs,
+    #         src_subset = surf2_idxs
+    #     ),
+    #     MultOp(MassOp(3, m[0], m[1][surf1_idxs]), mass_op_factor)
+    # ]))
 
     dof_pts = m[0][m[1][surf2_idxs]]
     dof_pts[:,:,1] -= dof_pts[0,0,1]
@@ -82,11 +107,7 @@ def regularized_tester(K, sep, continuity):
 
 
     slip_flat = slip.flatten()
-    ops[1].dot(slip_flat)
     outs = [o.dot(slip_flat) for o in ops]
-
-    # mass_op = MassOp(3, m[0], m[1][surf1_idxs])
-    # outs[1] -= 0.5 * mass_op.dot(slip_flat)
 
     if continuity:
         from tectosaur.constraint_builders import continuity_constraints, \
@@ -96,12 +117,14 @@ def regularized_tester(K, sep, continuity):
         cs.extend(free_edge_constraints(m[1][surf1_idxs]))
         cm, c_rhs = build_constraint_matrix(cs, outs[0].shape[0])
         final_outs = [cm.T.dot(v) for v in outs]
+        plot_outs = [cm.dot(v) for v in final_outs]
     else:
+        plot_outs = outs
         final_outs = outs
 
     should_plot = True
     if should_plot:
-        plot_fnc(m, surf1_idxs, surf2_idxs, slip, outs)
+        plot_fnc(m, surf1_idxs, surf2_idxs, slip, plot_outs)
 
     for i in range(len(final_outs)):
         for j in range(i + 1, len(final_outs)):
@@ -117,15 +140,20 @@ def test_regularized_A_farfield():
 def test_regularized_H_farfield():
     regularized_tester('H', 4.0, True)
 
-def test_regularized_nearfield():
-    nearfield_dist = 0.4
-    K_char = 'A'
-    if K_char == 'U':
-        return
-    regularized_tester(K_char, nearfield_dist, False)
+def test_regularized_T_nearfield():
+    regularized_tester('T', 0.4, False)
+
+def test_regularized_A_nearfield():
+    regularized_tester('A', 0.4, True)
+
+def test_regularized_H_nearfield():
+    regularized_tester('H', 0.4, True)
 
 def test_regularized_T_self():
-    regularized_tester('T', 0.0, False)
+    regularized_tester('T', 0.0, False, -0.5)
+
+def test_regularized_A_self():
+    regularized_tester('A', 0.0, True, 0.5)
 
 def test_regularized_H_self():
     regularized_tester('H', 0.0, True)
