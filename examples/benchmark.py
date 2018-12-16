4b822461f45b4447f1fec6b7d73e8fd2f78e5092
Ben Thompson <t.ben.thompson@gmail.com>
2018-12-16 02:28:16 -0500

    benchmark

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -4,22 +4,20 @@ import numpy as np
 import tectosaur as tct
 tct.logger.setLevel(logging.INFO)
 
-n = 100
+n = 50
 corners = [[-1.0, -1.0, 0], [-1.0, 1.0, 0], [1.0, 1.0, 0], [1.0, -1.0, 0]]
 m = tct.make_rect(n, n, corners)
 
 t = tct.Timer()
-op = tct.RegularizedSparseIntegralOp(
-    8, 8, 8, 2, 5, 2.5,
-    'elasticRT3', 'elasticRT3', [1.0, 0.25], m[0], m[1],
-    np.float32,
-    # farfield_op_type = tct.TriToTriDirectFarfieldOp,
-    farfield_op_type = tct.FMMFarfieldOp(mac = 4.5, pts_per_cell = 100)
+all_tris = np.arange(m[1].shape[0])
+op = tct.FMMFarfieldOp(mac = 4.5, pts_per_cell = 100)(
+    2, 'elasticRT3', [1.0, 0.25], m[0], m[1], np.float32, all_tris, all_tris
 )
 t.report('build op')
 
-x = np.random.rand(op.shape[1])
+x = np.random.rand(m[1].shape[0] * 9)
 t.report('x')
 
-y = op.dot(x)
-t.report('op.dot(x)')
+for i in range(1):
+    y = op.dot(x)
+    t.report('op.dot(x)')
